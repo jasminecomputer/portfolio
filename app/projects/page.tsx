8df3a740b61a01aa482bb9e390a263377ee9420d
Jasmine Chen <chenjas@mit.edu>
2024-09-24 21:36:59 -0400

    removed git repository from 4.032 final project and added link

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,9 @@
 //import { BlogPosts } from "app/components/posts"
 import Image from "next/image";
+import Script from "next/script";
+import Link from "next/link";
+import godfather_final from "./processes/godfather_final.png";
+import { ProjectProcesses } from "app/components/posts";
 
 export const metadata = {
   title: "Projects",
@@ -12,16 +16,28 @@ export default function Page() {
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Projects</h1>
       <div></div>
       <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-        Godfather Movie Data
+        Movie Data Project
         <p className="text-neutral-900 dark:text-neutral-100 tracking-tight"></p>
       </div>
       <Image
-        src="/projectsinfo/godfather_final.png"
+        src={godfather_final}
         alt="Bar Chart titled Distance When There is None"
         width={400}
         height={300}
       />
       {/*<BlogPosts />*/}
+      {/*<ProjectProcesses />*/}
+      <div> Amazon Data Project </div>
+
+      {/*<Script src="./4.032FinalProject/amazonIteration.js" />*/}
+      <a
+        href="/4.032FinalProject/index.html"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "blue", textDecoration: "underline" }}
+      >
+        web
+      </a>
     </section>
   );
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,5 @@
 import { getBlogPosts } from "app/blog/utils";
+import { getProjects } from "app/projects/utils";
 
 export const baseUrl = "https://portfolio-blog-starter.vercel.app";
 
@@ -6,6 +7,11 @@ export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
+  }));
+
+  let projects = getProjects().map((project) => ({
+    url: `${baseUrl}/blog/${project.slug}`,
+    lastModified: project.metadata.publishedAt,
   }));
 
   let routes = ["", "/writing", "/projects"].map((route) => ({
