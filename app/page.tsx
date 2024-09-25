54f299d3e9e553667923437998a9edd5d83554c2
Jasmine Chen <chenjas@mit.edu>
2024-09-24 23:27:18 -0400

    home changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,11 @@ export default function Page() {
         Jasmine Chen
       </h1>
       <p className="mb-4">
-        {`Hi! I'm Jasmine. I'm originally
-        from San Diego, CA, but now reside in Cambridge, MA. 
-        I recently graduated from MIT, where I studied 
-        Computation and Cognition. I like technology, design, 
+        {`Hi! I'm Jasmine, and I recently graduated from MIT, where I studied 
+        Computation and Cognition. I'm originally
+        from San Diego, CA, but now reside in Cambridge, MA. I created this website 
+        to document some past work and also motivate myself to create more.
+         I am interested in technology, design, 
         and cognition, in no particular order. If you 
         have similar interests or would like to know more, please reach out! 
         `}
